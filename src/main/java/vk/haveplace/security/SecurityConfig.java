616bc6981601ca67c4b7e14e5b18bc488de4c611
IgoryanNUK <supernuk27@mail.ru>
2025-09-20 10:16:42 +0300

    disable cors

--- src/main/java/vk/haveplace/security/SecurityConfig.java
+++ src/main/java/vk/haveplace/security/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").hasAnyRole("OWNER", "DEVELOPER")
                         .anyRequest().permitAll())
